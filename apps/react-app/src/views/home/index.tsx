146a4ad8ec104b320cef1f62f9fd715d193cd546
liuchengjin <75836368@qq.com>
2023-11-13 13:51:25 +0800

    feat: 前端添加默认导航菜单选中

--- apps/react-app/src/views/home/index.tsx
+++ apps/react-app/src/views/home/index.tsx
@@ -31,7 +31,7 @@ const items: MenuItem[] = [
 
 const Home: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false)
-  const [menuKey, setMenuKey] = useState('')
+  const [menuKey, setMenuKey] = useState('/user')
   const handleMenuClick = ({ key }: { key: string }) => {
     setMenuKey(key)
   }
@@ -47,7 +47,7 @@ const Home: React.FC = () => {
         <Menu
           theme="dark"
           onClick={handleMenuClick}
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['user']}
           mode="inline"
           items={items}
         />
