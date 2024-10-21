6c5ed83504211a368e5fece77fb363b67a816cc7
liuchengjin <75836368@qq.com>
2024-10-21 18:57:00 +0800

    ci: 部署文档

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://badlym.github.io/liuchengjin-monorepo/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/liuchengjin-monorepo/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
